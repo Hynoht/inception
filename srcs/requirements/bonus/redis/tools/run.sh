1ba80689282613ce4dfc9bb56e8413c32bc4c95f
Arthinew <dinandrianom@gmail.com>
2025-06-14 14:01:06 +0300

    delete

--- srcs/requirements/bonus/redis/tools/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-REDIS_PASSWORD=$(cat /run/secrets/redis_password)
-
-sed -i 's/^# *requirepass .*/requirepass '"$REDIS_PASSWORD"'/' /etc/redis/redis.conf
-
-exec redis-server /etc/redis/redis.conf
